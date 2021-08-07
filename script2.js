b36a236ed19295aa35eedb2af21178584b51061b
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-07 19:19:59 +0300

    lesson06 is ready

--- script2.js
+++ script2.js
@@ -1,51 +1,47 @@
 let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n)
 };
+// isNumber(userNum);
 
 const getRandomNumber = function (n, m) {
   const min = Math.ceil(Math.min(n, m));
   const max = Math.floor(Math.max(n, m));
 
-  return Math.floor(Math.random()*(max-min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-console.log(getRandomNumber(6, 12));
+// console.log(getRandomNumber(6, 12));
 
-// function getUserNumber(str) {
-//   const n = prompt('str');
-//   if (isNumber(n)) return + n;
 
-//   if (n === null) return null;
-// }
-// getUserNumber(str);
-
-function gameBot () {
+function gameBot() {
   const randomNum = getRandomNumber(1, 100);
- 
-  
   console.log(randomNum);
 
-     function checkNum () {
-      const userNum = prompt('Угадай число от 0 до 100');
+  function checkNum() {
+    const userNum = prompt('Угадай число от 0 до 100');
 
     if (userNum === null) return alert('Игра окончена');
-  
+
     if (userNum > randomNum) {
-    alert('Загаданное число меньше');
-    return checkNum;
-  }
+      alert('Загаданное число меньше');
+      return checkNum();
+    }
 
-  if (userNum < randomNum) {
-    alert('Загаданное число больше');
-    return checkNum;
-  }
+    if (userNum < randomNum) {
+      alert('Загаданное число больше');
+      return checkNum();
+    }
 
-  if (userNum === isNumber) {
-    alert('Введи число!');
-    return checkNum;
-  }
+    if (!isNumber(userNum)) {
+      alert('Введи число!');
+      return checkNum();
+    }
 
-  if (userNum === randomNum) {
-    alert('Поздравляю, Вы угадали!!!');
+    if (userNum == randomNum) {
+      alert('Поздравляю, Вы угадали!!!');
+    }
   }
+  
+  checkNum();
 }
-gameBot();
+  gameBot();
+ 
